9926a84b92e40db41044fde642d2c06adeff7812
Zaray <cor191025@uvg.edu.gt>
2021-04-22 02:45:05 -0600

    basic test config

---
+++ src/components/calculator.test.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { screen, render, cleanup, getByTestId } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import Calculator from './calculator';
+
